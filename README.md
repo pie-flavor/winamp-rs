e06cf72d9d463e04a942facb5c5b583cf8d186d4
Adam Spofford <pieflavor.mc@gmail.com>
2020-07-02 06:18:21 -0700

    Prepare to publish

---
+++ README.md
@@ -0,0 +1,4 @@
+# winamp-sys
+
+This crate provides FFI bindings for the Winamp 2 plugin interface.
+All modules except `wa_ipc` must be enabled by crate feature: `dsp`, `gen`, `in`, `ipc-pe`, `out`, and `wa-dlg`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(nonstandard_style)]
 #![deny(missing_copy_implementations, missing_debug_implementations)]
+#![cfg_attr(doc, feature(external_doc))]
+#![cfg_attr(doc, doc(include = "../README.md"))]
 
 #[cfg(not(all(target_arch = "x86", windows)))]
 compile_error!("The only supported platform is Windows 32-bit");
